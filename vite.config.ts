51dccd1f091be145fe5b30614c0295446aea547e
Naveen Kumar <text.nav@gmail.com>
2025-09-20 21:15:03 +0800

    feat: Remove Linaria dependencies and related Button component

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import linaria from '@linaria/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    linaria({
-      sourceMap: true,
-      displayName: true,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
